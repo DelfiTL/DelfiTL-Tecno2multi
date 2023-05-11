0ae630e465fe7e67bdda5722ff09368a8db15251
DelfiTL <delfitrebino@hotmail.com>
2023-05-11 11:15:46 -0300

    nueva pestaña

--- sketch.js
+++ sketch.js
@@ -1,14 +1,26 @@
+let x=-10; //posx de las figuras
 
-let x=-10;
+/*PImage en p5js
+#necesito una varible para el nombre de las img y otra para el array, desp le asigno img al array
+function preload() {} aca sucede la magia
+un For(){} para cargar las img  por el nombre_00
+*/
+function preload() { //con esta funcion carga las img
+
+
+}
 
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
 
+
+
+
 }
 
 function draw() {
-  background (200);
+  background (255,250,200);
 
 angleMode(DEGREES); //cambia a angulo
 
@@ -16,43 +28,4 @@ pantalla1();
 pantalla2 ();
 moverx ();
 
-}
-function moverx(){
-  if (x<=1500){
-    x+=1;
-    } else if (x>=1500){
-      x=-10;
-    }
-}
-
-function pantalla1() {
-  let a =( 40/-mouseY );  //el número q divido me da el ángulo en comparación a la distancia q muevo el mouse, mas alto el numero mas limito el angulo
-push();
-translate(0,height);
-rotate(PI/a);
-rectMode (CENTER);
-rect(x-120, 40, 120, 20); 
-rect(x, 0, 80, 20); 
-rect(x, 30, 10, 5); 
-rect(x, 60, 10, 5); 
-rect(x, -200, 50, 5); 
-pop();
-}
-
-function pantalla2 (){
- // let b = (200/mouseX);
- let b = (50/mouseX);
-  push();
-  translate(width, height);
-
-  rotate(PI/b);
-  
-   rectMode (CENTER);
-  rect(-x-120, 40, 120, 20); 
-  rect(-x, 0, 80, 20); 
-  rect(-x, 30, 10, 5); 
-  rect(-x, 60, 10, 5); 
-  rect(-x, -200, 50, 5); 
-  pop();
-  
 }
