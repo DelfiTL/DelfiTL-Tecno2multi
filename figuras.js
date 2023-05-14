b9cbaa04376ca79283093c3f3b427aaa73d02db5
DelfiTL <delfitrebino@hotmail.com>
2023-05-14 18:32:14 -0300

    confirmado

--- figuras.js
+++ figuras.js
@@ -1,12 +1,12 @@
 function moverx(){
     if (x<=1500){
-      x+=1;
+      x+=10;
       } else if (x>=1500){
         x=-10;
       }
   }
 
-  
+
   
   function pantalla1() {
     let a =( 40/-mouseY );  //el número q divido me da el ángulo en comparación a la distancia q muevo el mouse, mas alto el numero mas limito el angulo
@@ -24,14 +24,18 @@ function moverx(){
   
   function pantalla2 (){
   
-   let b = (50/mouseX);
+   let b = (50/mouseY); //original mouseX
+   let rotafig =(50/mouseX); // menor numero q divide mas amplitud de circunferencia
     push();
     translate(width, height);
   
     rotate(PI/b);
     
      rectMode (CENTER);
-    rect(-x-120, 40, 120, 20); 
+    push ();
+     rotate (PI/rotafig); // se mueve con el mouseX
+     rect(-x-120, 40, 120, 20); 
+    pop ();
     rect(-x, 0, 80, 20); 
     rect(-x, 30, 10, 5); 
     rect(-x, 60, 10, 5); 
--- sketch.js
+++ sketch.js
@@ -1,4 +1,11 @@
 let x=-10; //posx de las figuras
+//var otra forma de declarar variable
+/*falta:
+#agregar las imagenes y cargarlas a un arreglo
+#agregar variacion de tam y/o  color 
+#cambio de saturacion
+#cual va a ser la interraccion
+*/
 
 /*PImage en p5js
 #necesito una varible para el nombre de las img y otra para el array, desp le asigno img al array
@@ -15,8 +22,6 @@ function setup() {
   createCanvas(windowWidth,windowHeight);
 
 
-
-
 }
 
 function draw() {
@@ -27,5 +32,9 @@ angleMode(DEGREES); //cambia a angulo
 pantalla1();
 pantalla2 ();
 moverx ();
+}
 
+
+function mouseClicked(){
+ 
 }
