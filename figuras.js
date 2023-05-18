ca783d680dc9ffe87de8bfb049ee78d38f9af82a
DelfiTL <103791308+DelfiTL@users.noreply.github.com>
2023-05-18 11:49:19 -0300

    Add files via upload

--- figuras.js
+++ figuras.js
@@ -1,6 +1,6 @@
 function moverx(){
     if (x<=1500){
-      x+=10;
+      x+=4;
       } else if (x>=1500){
         x=-10;
       }
@@ -10,15 +10,50 @@ function moverx(){
   
   function pantalla1() {
     let a =( 40/-mouseY );  //el número q divido me da el ángulo en comparación a la distancia q muevo el mouse, mas alto el numero mas limito el angulo
-  push();
+    let rotafig =(-50/mouseX);
+  
+    push();
   translate(0,height);
   rotate(PI/a);
   rectMode (CENTER);
-  rect(x-120, 40, 120, 20); 
-  rect(x, 0, 80, 20); 
-  rect(x, 30, 10, 5); 
+  fill (215,36,15)
+  rect(x, 100, 150, 150); //cuadrado rojo grande
+  rect(x-220, 40, 20, 20);  
+  rect(x-40, 0, 80, 20); 
+
+  fill (200, 80,10); //naranjas
+  rect(x-500, 130, 10, 5); 
+  rect(x-450, -200, 300, 5); 
+  rect(x-600, 50, 50, 3); 
+  rect(x-600, 130, 10, 5); 
+  rect(x-450, 200, 80, 50); 
+  rect(x-1000, 50, 450, 30); 
+
+
+  fill (33, 64,120); //azul
+  rect(x, -400, 80, 50); 
+  rect(x-10, 30, 10, 5); 
+  rect(x, -300, 300, 5); 
+  rect(x, -50, 500, 1); 
+  rect(x-400, 100, 80, 50); 
+  rect(x-700, 150, 40, 30)
+  
+  fill (0);
+  rect(x-600, 40, 120, 20);  
+  rect(x, 300, 150, 20); 
+  rect(x, 500, 40, 5); 
+  rect(-x, -200, 50, 5); 
+rect(-x, -2500, 10, 5); 
+rect(-200-x, -1500, 10, 5); 
+
+  push ();
+  rotate (PI/rotafig);
   rect(x, 60, 10, 5); 
   rect(x, -200, 50, 5); 
+  fill (33, 64,120); //azul
+  rect(x, -400, 80, 50);
+pop ();
+
 
   pop();
   }
@@ -26,23 +61,41 @@ function moverx(){
   function pantalla2 (){
   fill (200,10,0);
   noStroke ();
-   let b = (50/mouseY); //original era mouseX
+   let b = (400/mouseY); //si el numero es negativo gira en contra del sentido del reloj y positivo a favor del reloj
    let rotafig =(50/mouseX); // menor numero q divide mas amplitud de circunferencia
     push();
-   translate(width, height);
+   translate(width, height/2);
     rotate(PI/b);
     
      rectMode (CENTER);
     push ();
+    
      rotate (PI/rotafig); // se mueve con el mouseX
-     rect(-x-120, 40, 120, 20); 
+     rect(-x-120, 40, 120, 20);  //los valos de X son negativos para q se muevan hacia la izquierda
+     rect(-x, 160, 10, 5);
     pop ();
     rect(-x, 0, 80, 20); 
-    fill (2,0,200);
+
+    fill (33, 64,120); //figuras azules
     rect(-x, 30, 10, 5); 
-    rect(-x, 60, 10, 5);
-    fill (0); 
+  
+    rect(-x, 30, 10, 5); 
+    rect(-x, 350, 100, 5);
+    rect(-x+550, 30, 10, 5); 
+    rect(-x+600, 350, 100, 5);
+    rect(-x+550, 120, 50, 50); 
+    rect(-x+450, 90, 10, 5);
+
+    fill (0);  //figuras negras
+    rect (-x, 100, 250,50)
     rect(-x, -200, 50, 5); 
+    rect(-x, 200, 10, 5); 
+    rect(-x, 100, 10, 5); 
+    rect (-x+700, 100, 70,50)
+    rect(-x+250, -200, 10, 5); 
+    rect(-x+400, 50, 10, 5); 
+    rect(-x+400, 100, 10, 5); 
+    rect(-x+450, 20, 200, 5); 
     pop();
     
   }
--- sketch.js
+++ sketch.js
@@ -14,6 +14,7 @@ un For(){} para cargar las img  por el nombre_00
 */
 
 let x=-10; //posx de las figuras
+
 //let FiguraAmarilla = [];
 //let cant=10; //para el for de las img
 let img;
@@ -42,10 +43,13 @@ angleMode(DEGREES); //cambia a angulo
 
 pantalla1();
 pantalla2 ();
-moverx ();
+
 }
 
 
 function mouseClicked(){
  
+}
+function mouseMoved() {
+  moverx ();
 }
